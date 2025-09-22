eefebe2e7dc018c7679a35c7774fbf885a5bc87a
Petricore25 <petricore25@gmail.com>
2025-09-22 12:41:49 +0200

    Fast Modular Exponentiation

--- Fast_Exponentiation/main.cpp
+++ Fast_Exponentiation/main.cpp
@@ -1,12 +1,8 @@
-constexpr int mod=1e9+7;
-
-long long fast_exp(long long base, long long exp)
-{
+long long fast_exp(long long base, long long exp, long long mod) {
     long long ans=1LL;
     base%=mod;
     if(base<0) base+=mod;
-    while(exp>0)
-    {
+    while(exp) {
         if(exp&1) ans=(ans*base)%mod;
         base=(base*base)%mod;
         exp>>=1;
